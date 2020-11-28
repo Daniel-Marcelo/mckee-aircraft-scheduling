da574ff5c8919f8efad1f6adf0ff0edd519a7707
Daniel <danielm.mckee@gmail.com>
2020-11-28 11:28:03 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mckee-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'aircraft-scheduling-app';
+}
