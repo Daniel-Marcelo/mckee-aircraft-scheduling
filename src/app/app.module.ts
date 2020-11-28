1a7c524e56083e224da244887f7663cd0f70c77b
Daniel <danielm.mckee@gmail.com>
2020-11-28 12:51:26 +0000

    initial setup and layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AircraftSchedulingModule } from './aircraft-scheduling/aircraft-scheduling.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AircraftSchedulingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
